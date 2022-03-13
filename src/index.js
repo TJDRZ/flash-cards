ed741a6c3f7ca117beece18d1e6fb74a146607cd
TJ <tjdrzewiecki@gmail.com>
2022-03-12 19:20:23 -0500

    WIP

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-import cards from "./cards.json";
+import jsonCards from "./cards.json";
+import "./reset.css";
+import "./style.css";
 
-for(let card in cards) {
-    console.log(card);
+const cardElement = document.querySelector(".card");
+const buttonElement = document.querySelector(".next");
+
+const cards = Object.entries(jsonCards);
+let currentCardIndex = 0;
+
+function randomNumber(min, max) {
+  return Math.round(Math.random() * (max - min));
 }
+
+cardElement.addEventListener("click", () => {
+  cardElement.innerText = "";
+  for (let entry in cards[currentCardIndex][1]) {
+    const pElement = document.createElement("p");
+    pElement.innerText = `${cards[currentCardIndex][1][entry]}`;
+    cardElement.appendChild(pElement);
+  }
+});
+
+function drawCard() {
+  currentCardIndex = randomNumber(1, cards.length);
+  cardElement.innerText = cards[currentCardIndex][0];
+}
+
+buttonElement.addEventListener("click", () => {
+  drawCard();
+});
+
+drawCard();
